6c3427dbfaa05e1ba62296cd7977bc5b74a9c7f9
Anish Jagdale (A.J) <76979541+root-aj000@users.noreply.github.com>
2025-07-16 13:44:57 +0530

    Update blog_schema.ts

--- src/content/schema/blog_schema.ts
+++ src/content/schema/blog_schema.ts
@@ -1,1 +1,11 @@
-.
+import { z } from 'astro:content';
+
+export const blogPostSchema = z.object({
+  contentType: z.literal('blog-post'),
+  title: z.string(),
+  author: z.string(),
+  date: z.date(),
+  serviceName: z.string(),
+  tags: z.array(z.string()).optional(),
+  description: z.string().optional(),
+});
