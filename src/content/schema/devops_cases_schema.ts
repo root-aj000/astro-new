775cf4ba6e2a0c4406b299b6b846ffb9a99d48ee
root-empire-aj <infous.seo@gmail.com>
2025-07-19 11:10:58 +0530

    pagination

--- src/content/schema/devops_cases_schema.ts
+++ src/content/schema/devops_cases_schema.ts
@@ -1,16 +1,15 @@
-// src/schemas/customMLBlog.ts
+import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 export const DevopsCases = defineCollection({
-  type: 'content',
-  schema: z.object({
-    title: z.string(),
-    contentType: z.literal('case-study'),
-    serviceName:z.literal('devops'),
-    date: z.date(),
-    author: z.string().optional(),
-    tags: z.array(z.string()).optional(),
-    description: z.string().optional(),
-    draft: z.boolean().default(false).optional(),
-  }),
+	// Load Markdown and MDX files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog/', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) => z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: image().optional(),
+	}),
 });
