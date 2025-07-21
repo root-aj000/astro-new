47ef64cbf1801bc1d1a411b6b729904fb16d2197
root-empire-aj <infous.seo@gmail.com>
2025-07-21 20:47:48 +0530

    added feature section for the services page, over

--- src/types/components.ts
+++ src/types/components.ts
@@ -1,61 +1,61 @@
 //  Global Component Prop Types in a Separate File
 export interface HeroProps {
-  hero : {
-    mh_line : string;
-    tg_line : string;
-    discription : string;
-    p_cta : string;
-    s_cta : string;
-    image : string;
-    alt : string;
+  hero: {
+    mh_line: string;
+    tg_line: string;
+    discription: string;
+    p_cta: string;
+    s_cta: string;
+    image: string;
+    alt: string;
   };
 }
 
 export interface Hero_2Props {
-  hero_2 : {
-    mh_line : string;
-    tg_line : string;
-    discription : string;
-    p_cta : string;
-    s_cta : string;
-    image : string;
-    alt : string;
+  hero_2: {
+    mh_line: string;
+    tg_line: string;
+    discription: string;
+    p_cta: string;
+    s_cta: string;
+    image: string;
+    alt: string;
   };
 }
 
 export interface Hero_3Props {
-  hero_3 : {
-    mh_line : string;
-    tg_line : string;
-    discription : string;
-    p_cta : string;
-    s_cta : string;
-    image : string;
-    alt : string;
+  hero_3: {
+    mh_line: string;
+    tg_line: string;
+    discription: string;
+    p_cta: string;
+    s_cta: string;
+    image: string;
+    alt: string;
   };
 }
 
 export interface Hero_4Props {
-  hero_4 : {
-    mh_line : string;
-    tg_line : string;
-    discription : string;
-    p_cta : string;
-    s_cta : string;
-    image : string;
-    alt : string;
+  hero_4: {
+    mh_line: string;
+    tg_line: string;
+    discription: string;
+    p_cta: string;
+    s_cta: string;
+    image: string;
+    alt: string;
   };
 }
 
 export interface logo_cloudProps {
-  logo_cloud:{
-    title : string;
-    logos : string[];
+  logo_cloud: {
+    title: string;
+    logos: string[];
   };
 }
 
 export interface HowItWorksProps {
-  Howitworks:{
+  Howitworks: {
     title: string;
     subtitle: string;
     image: string;
@@ -64,8 +64,8 @@ export interface HowItWorksProps {
       desc: string;
       img: string;
       caption: string;
-  }[];
-};
+    }[];
+  };
 }
 
 export interface ForWhomItem {
@@ -75,34 +75,34 @@ export interface ForWhomItem {
 }
 
 export interface ForWhomBlock {
-  forwhom: { 
+  forwhom: {
     title: string;
     description: string;
     p_cta: string;
-    items: ForWhomItem[]; 
-  }; 
+    items: ForWhomItem[];
+  };
 }
 
 export interface list_headerProps {
-  list_header : {
-    title : string;
-    discription : string;   
+  list_header: {
+    title: string;
+    discription: string;
   };
 }
 
 export interface FeatureItem {
-    title: string;
-    description: string;
-    cta: string;
-    link: string;
-    icon: string;
+  title: string;
+  description: string;
+  cta: string;
+  link: string;
+  icon: string;
 }
 
 export interface FeatureGridProps {
-  grid:{
-  title: string;
-  subtitle: string;
-  features: FeatureItem[];
+  grid: {
+    title: string;
+    subtitle: string;
+    features: FeatureItem[];
   };
 }
 
@@ -112,7 +112,7 @@ export interface StatItem {
 }
 
 export interface StatsProps {
-  stats:{
+  stats: {
     heading: string;
     description: string;
     items: StatItem[];
@@ -120,7 +120,7 @@ export interface StatsProps {
 }
 
 export interface SubscribeFormProps {
-  subscribe:{
+  subscribe: {
     title: string;
     description: string;
     placeholder: string;
@@ -144,33 +144,55 @@ export interface BlogCard {
 }
 
 export interface Home_header_blogProps {
-  home_header_blog:{
+  home_header_blog: {
     title: string;
     subtitle: string;
   };
 }
 
 export interface Home_header_casesProps {
-  home_header_cases:{
+  home_header_cases: {
     title: string;
     subtitle: string;
   };
 }
-export interface IndexProps{
-  indexpro:{
-    hero: HeroProps['hero'];
-    cloud_data: logo_cloudProps['logo_cloud'];
-    hero_2: Hero_2Props['hero_2'];
-    hero_3: Hero_3Props['hero_3'];
-    hero_4: Hero_4Props['hero_4'];
-    howitworks: HowItWorksProps['Howitworks'];
-    forwhom: ForWhomBlock['forwhom'];
-    list_header: list_headerProps['list_header'];
-    grid: FeatureGridProps['grid'];
-    stats: StatsProps['stats'];
-    subscribe: SubscribeFormProps['subscribe'];
-    blog_data: BlogCardProps;
-    Home_header_blog: Home_header_blogProps['home_header_blog'];
-    Home_header_cases: Home_header_casesProps['home_header_cases'];
+
+export interface FeatureLRProps {
+  featurelr: {
+    title: string;
+    subtitle: string;
+    image: string;
+    LeftItems: {
+      title: string;
+      description: string;
+      icon: string;
+    }[];
+    RightItems: {
+      title: string;
+      description: string;
+      icon: string;
+    }[];
+      
   };
 }
+export interface IndexProps {
+  indexpro: {
+    hero: HeroProps["hero"];
+    cloud_data: logo_cloudProps["logo_cloud"];
+    hero_2: Hero_2Props["hero_2"];
+    hero_3: Hero_3Props["hero_3"];
+    hero_4: Hero_4Props["hero_4"];
+    howitworks: HowItWorksProps["Howitworks"];
+    forwhom: ForWhomBlock["forwhom"];
+    list_header: list_headerProps["list_header"];
+    grid: FeatureGridProps["grid"];
+    stats: StatsProps["stats"];
+    subscribe: SubscribeFormProps["subscribe"];
+    blog_data: BlogCardProps;
+    Home_header_blog: Home_header_blogProps["home_header_blog"];
+    Home_header_cases: Home_header_casesProps["home_header_cases"];
+    featurelr: FeatureLRProps["featurelr"];
+  };
+}
+
+
