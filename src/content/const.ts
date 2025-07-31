82582511721bce3f277aa413b8257d4e0761adbc
root-empire-aj <infous.seo@gmail.com>
2025-07-31 13:48:10 +0530

    resolve error of // in CC

--- src/content/const.ts
+++ src/content/const.ts
@@ -1,22 +1,22 @@
 // const BasePath ="abc"; setting up here 
 
 export const BASE_PATH_CONST = {
-    Aichatbot_blog: "./services/ai-chatbot/blog",
-    Aichatbot_case: "./services/ai-chatbot/cases/page",
-    CustomML_blog: './services/custom-ml/blog/page',
-    CustomML_case: './services/custom-ml/cases/page',
-    CloudComputing_blog: './services/cloud-computing/blog/page',
-    CloudComputing_case: './services/cloud-computing/cases/page',
-    Devops_blog: './services/devops/blog/page',
-    Devops_case: './services/devops/cases/page',
+    Aichatbot_blog: "/services/ai-chatbot/blog",
+    Aichatbot_case: "/services/ai-chatbot/cases/page",
+    CustomML_blog: '/services/custom-ml/blog',
+    CustomML_case: '/services/custom-ml/cases',
+    CloudComputing_blog: '/services/cloud-computing/blog/page',
+    CloudComputing_case: '/services/cloud-computing/cases/page',
+    Devops_blog: '/services/devops/blog/page',
+    Devops_case: '/services/devops/cases/page',
 };
 export const BASE_PATH_BLOG = {
     Aichatbot_blog: "/services/ai-chatbot/blog",
     Aichatbot_case: "/services/ai-chatbot/cases",
-    CustomML_blog: '/services/custom-ml/blog/',
-    CustomML_case: '/services/custom-ml/cases/',
-    CloudComputing_blog: '/services/cloud-computing/blog/',
-    CloudComputing_case: '/services/cloud-computing/cases/',
-    Devops_blog: '/services/devops/blog/',
-    Devops_case: '/services/devops/cases/',
+    CustomML_blog: '/services/custom-ml/blog',
+    CustomML_case: '/services/custom-ml/cases',
+    CloudComputing_blog: '/services/cloud-computing/blog',
+    CloudComputing_case: '/services/cloud-computing/cases',
+    Devops_blog: '/services/devops/blog',
+    Devops_case: '/services/devops/cases',
 };
