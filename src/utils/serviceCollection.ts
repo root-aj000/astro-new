d2b0002a2027d56a86419e04d53fef809898b0cb
Anish Jagdale (A.J) <76979541+root-aj000@users.noreply.github.com>
2025-08-10 10:25:36 +0530

    Update serviceCollection.ts

--- src/utils/serviceCollection.ts
+++ src/utils/serviceCollection.ts
@@ -1,7 +1,13 @@
-// src/utils/serviceCollections.ts
 export const serviceToCollectionMap: Record<string, string> = {
   "ai-chatbot": "AichatbotBlog",
-  "nlp": "cloud-computing",
+  "nlp": "NLPBlog",
   "cv": "ComputerVisionBlog",
-  // add all services here
+  // Add other blog collections per service
+};
+
+export const serviceToCasesCollectionMap: Record<string, string> = {
+  "ai-chatbot": "AichatbotCases",
+  "nlp": "NLPCases",
+  "cv": "ComputerVisionCases",
+  // Add other case study collections per service
 };
