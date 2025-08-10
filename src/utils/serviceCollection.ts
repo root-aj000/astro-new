617636c1e08aee4cff2d1188d8d326d9d8b7c10c
Anish Jagdale (A.J) <76979541+root-aj000@users.noreply.github.com>
2025-08-10 10:23:39 +0530

    Create serviceCollection.ts

---
+++ src/utils/serviceCollection.ts
@@ -0,0 +1,7 @@
+// src/utils/serviceCollections.ts
+export const serviceToCollectionMap: Record<string, string> = {
+  "ai-chatbot": "AichatbotBlog",
+  "nlp": "cloud-computing",
+  "cv": "ComputerVisionBlog",
+  // add all services here
+};
